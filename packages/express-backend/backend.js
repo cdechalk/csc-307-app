405cce21077f273a98a21a31913fe9c4855495c1
christinadechalk <cdechalk@calpoly.edu>
2024-10-01 18:52:25 -0700

    third commit (step 4 done)

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,10 +4,23 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
 app.use(express.json());
 
 app.get("/users", (req, res) => {
-  res.send(users);
+  const name = req.query.name;
+  if(name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
 });
 
 app.listen(port, () => {
